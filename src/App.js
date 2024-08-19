239fc2ea2b53fa571c328748c322434672f8769d
Hem Korat <62368454+Hem9025@users.noreply.github.com>
2024-08-19 13:15:54 +1200

    App.js File to work for routing
    I was deleted from main due some reason so I am uploading directly.

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col } from "react-bootstrap";
+import { Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { UserAuthContextProvider } from "./context/UserAuthContext";
+
+function App() {
+  return (
+    <UserAuthContextProvider>
+      <Container>
+        <Row>
+          <Col>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/customer" element={<Dashboard />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
+    </UserAuthContextProvider>
+  );
+}
+
+export default App;

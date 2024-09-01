47ecd9a92f05a2a1dbd5c0f511e1ea076b573657
Hem Korat <hemkorat400@gmail.com>
2024-09-01 23:55:55 +1200

    User Profile And My Listing

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import { Link } from 'react-router-dom';
+import UserProfile from './UserProfile';
 
 function Dashboard() {
     return (
       <div>
         <Header/>
           <h2>Welcome to the Dashboard!</h2>
-      
+          <UserProfile/>
           <h4>Add New <Link to="/addproduct">Products</Link></h4>
         <Footer/>
     </div>
